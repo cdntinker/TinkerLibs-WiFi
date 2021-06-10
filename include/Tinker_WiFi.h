31681652852916398f1d76beb7c919d91a3e3dcd
cdntinker <58090022+cdntinker@users.noreply.github.com>
2021-06-10 15:48:01 -0400

    Add files via upload

---
+++ include/Tinker_WiFi.h
@@ -0,0 +1,19 @@
+#include <NOTARDUINO.h>
+
+#if defined(ESP8266)
+    #include "ESP8266WiFi.h"
+    #include "ESPAsyncTCP.h"
+    #include "flash_hal.h"
+#elif defined(ESP32)
+    #include "WiFi.h"
+    #include "AsyncTCP.h"
+    #include "Update.h"
+    #include "esp_int_wdt.h"
+    #include "esp_task_wdt.h"
+#endif
+
+String ip3string(IPAddress ip);
+String processor(const String &var);
+
+void setup_wifi();
+int WiFi_strength();
---
+++ src/WiFi_Functions.cpp
@@ -0,0 +1,81 @@
+#include <Tinker_DEBUG.h>
+#include "Tinker_WiFi.h"
+
+// #include "TopSecret.h"
+extern const char* host;
+extern const char *ssid;
+extern const char *password;
+
+extern char DEBUGtxt[48];
+
+void setup_wifi() //   We start by connecting to a WiFi network
+{
+  WiFi.hostname(host);
+
+    delay(10);
+
+    DEBUG_Init("WiFi");
+
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    sprintf(DEBUGtxt, "Connecting to: %-22s", ssid);
+    DEBUG_SectionTitle(DEBUGtxt);
+    {
+        DEBUG_ProgressBar0();
+        int dotcount = 0;
+        while (WiFi.status() != WL_CONNECTED) // Give it a bit of time to establish the WiFi connection...
+        {
+            dotcount++;
+            delay(500);
+            DEBUG_ProgressBar1();
+            if (dotcount >= 46)
+            {
+                DEBUG_ProgressBar2(dotcount);
+                dotcount = 0;
+                DEBUG_ProgressBar0();
+            }
+            // SSD1306_Static("  no WiFi  ", 3);
+        }
+        DEBUG_ProgressBar2(dotcount);
+    } // while (WiFi.status() != WL_CONNECTED)
+
+    randomSeed(micros());
+    DEBUG_Success("WiFi connected");
+    sprintf(DEBUGtxt,
+            "IP address:  %-16s",
+            WiFi.localIP().toString().c_str());
+    DEBUG_LineOut(DEBUGtxt);
+    sprintf(DEBUGtxt,
+            "MAC address: %-18s",
+            WiFi.macAddress().c_str());
+    DEBUG_LineOut(DEBUGtxt);
+}
+
+///////////////////////
+
+int WiFi_strength()
+{
+    return (WiFi.RSSI());
+}
+
+int blip[3] = {000, 255, 000};  // GRN
+
+void WiFi_Test()  // Is Good...  Maybe
+{
+    if (WiFi.status() != WL_CONNECTED)
+    {
+        Serial.println("Oh Poop!");
+        blip[0] = 128;
+        blip[1] = 128;
+    }
+    else
+    {
+    if (blip[1] == 255)
+        blip[1] = 0;
+    else
+        blip[1] = 255;
+    }
+#ifdef d_Pixels
+    SetAPixel(3, blip);
+#endif // d_Pixels
+}
