f29035e48c6e75068f109424265415b08f9c2175
cdntinker <cdntinker@gmail.com>
2021-06-11 15:08:55 -0400

    workin on DEBUG_ProgressBar

--- src/WiFi_Functions.cpp
+++ src/WiFi_Functions.cpp
@@ -28,7 +28,7 @@ void setup_wifi() //   We start by connecting to a WiFi network
             dotcount++;
             delay(500);
             DEBUG_ProgressBar1();
-            if (dotcount >= 63)
+            if (dotcount >= 62)
             {
                 DEBUG_ProgressBar2(dotcount);
                 dotcount = 0;
