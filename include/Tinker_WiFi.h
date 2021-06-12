0362baeb82464c6c82f95a4a23b9ea845cda7e46
cdntinker <cdntinker@gmail.com>
2021-06-12 17:35:25 -0400

    llll

--- include/Tinker_WiFi.h
+++ include/Tinker_WiFi.h
@@ -8,14 +8,14 @@
 
 #if defined(ESP8266)
     #include "ESP8266WiFi.h"
-    #include "ESPAsyncTCP.h"
-    #include "flash_hal.h"
+    // #include "ESPAsyncTCP.h"
+    // #include "flash_hal.h"
 #elif defined(ESP32)
     #include "WiFi.h"
-    #include "AsyncTCP.h"
-    #include "Update.h"
-    #include "esp_int_wdt.h"
-    #include "esp_task_wdt.h"
+    // #include "AsyncTCP.h"
+    // #include "Update.h"
+    // #include "esp_int_wdt.h"
+    // #include "esp_task_wdt.h"
 #endif
 
 String ip3string(IPAddress ip);
