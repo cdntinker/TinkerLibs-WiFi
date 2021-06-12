71c3925050ba820fba9cfddce97f3bf0c4a9a265
cdntinker <58090022+cdntinker@users.noreply.github.com>
2021-06-12 19:59:53 -0400

    Update WiFi_Functions.cpp

--- src/WiFi_Functions.cpp
+++ src/WiFi_Functions.cpp
@@ -1,8 +1,6 @@
 #include <Tinker_DEBUG.h>
 #include "Tinker_WiFi.h"
 
-// #include "TopSecret.h"
-
 extern const char *host;
 extern const char *ssid;
 extern const char *password;
@@ -22,15 +20,14 @@ void setup_WiFi() //   We start by connecting to a WiFi network
     sprintf(DEBUGtxt, "Connecting to: %-22s", ssid);
     DEBUG_SectionTitle(DEBUGtxt);
     {
-        // DEBUG_ProgressBar0();
         int dotcount = 0;
         while (WiFi.status() != WL_CONNECTED) // Give it a bit of time to establish the WiFi connection...
         {
             delay(500);
             dotcount = DEBUG_ProgressBar(dotcount);
-        }
+        } // while (WiFi.status() != WL_CONNECTED)
         DEBUG_ProgressBar2(dotcount);
-    } // while (WiFi.status() != WL_CONNECTED)
+    }
 
     randomSeed(micros());
     DEBUG_Success("WiFi connected");
