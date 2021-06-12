c8cb121479fbab5069a65c544e920ebeb6dc6574
cdntinker <58090022+cdntinker@users.noreply.github.com>
2021-06-12 19:58:27 -0400

    Update Tinker_WiFi.h

--- include/Tinker_WiFi.h
+++ include/Tinker_WiFi.h
@@ -3,19 +3,10 @@
 
 // Stuff only needed in WiFi_functions.cpp goes here.
 
-// #include <NOTARDUINO.h>
-// #include <Arduino.h>
-
 #if defined(ESP8266)
     #include "ESP8266WiFi.h"
-    // #include "ESPAsyncTCP.h"
-    // #include "flash_hal.h"
 #elif defined(ESP32)
     #include "WiFi.h"
-    // #include "AsyncTCP.h"
-    // #include "Update.h"
-    // #include "esp_int_wdt.h"
-    // #include "esp_task_wdt.h"
 #endif
 
 String ip3string(IPAddress ip);
